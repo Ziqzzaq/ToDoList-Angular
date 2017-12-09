23e1755c1aa72fb680a64c9fbf93409f57bf75be
Michał Borzęcki <ziqzzaq.web@gmail.com>
2017-12-09 10:17:25 +0100

    Delete variable 'o' type Observable<any>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent implements OnInit {
   newTask: string;
   tasksList: Array<string> = [];
   doneTasksList: Array<string> = [];
-  o: Observable<any>;
   add() {
     this.tasksList.push(this.newTask);
     this.newTask = '';
@@ -39,14 +38,12 @@ export class AppComponent implements OnInit {
     this.storage.setItem('doneTasks', this.doneTasksList).subscribe( () => {}, () => {});
   }
   loadTasksList() {
-      this.o = this.storage.getItem('tasks');
-      this.o.subscribe((someArray: any[]) => {
+      this.storage.getItem('tasks').subscribe((someArray: any[]) => {
           if (someArray.length > 0) {
               this.tasksList = someArray;
           }
       });
-      this.o = this.storage.getItem('doneTasks');
-      this.o.subscribe((someArray: any[]) => {
+      this.storage.getItem('doneTasks').subscribe((someArray: any[]) => {
           if (someArray.length > 0) {
               this.doneTasksList = someArray;
           }
